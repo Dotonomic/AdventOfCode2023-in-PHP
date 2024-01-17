c6449556270b628a35fd7affb128f1c20f4f0cef
Dotonomic <github@dotonomic.com>
2024-01-17 12:15:02 +0000

    Update 10.php

--- 10.php
+++ 10.php
@@ -27,21 +27,19 @@ $found = FALSE;
 
 foreach (file("10input.txt") as $lineNumber => $line) {
 	$lines[] = $line;
-	
-	if (!$found)
-		for ($i=0;$i<140;$i++)
-			if (substr($line,$i,1) == "S") {
-				define("s",[$i,$lineNumber]);
-				$found = TRUE;
-				break;
-			}
+
+	for ($i=0;$i<140;$i++)
+		if (substr($line,$i,1) == 'S') {
+			define("s",[$i,$lineNumber]);
+			$found = TRUE;
+			break 2;
+		}
 }
 
 $current = [s[0],s[1]-1]; $dir = [0,-1]; //CHEATING
 $steps = 1;
 
-$loop = [];
-$loop[s[0]][s[1]-1] = '';
+$loop[s[0]][s[1]-1] = 0;
 $minX = s[0];
 $maxX = s[0];
 $minY = s[1];
@@ -52,7 +50,7 @@ while ($current != s) {
 	$char = substr($lines[$current[1]],$current[0],1);
 	$dir = newDir($char,$dir);
 	$current = add($current,$dir);
-	$loop[$current[0]][$current[1]] = '';
+	$loop[$current[0]][$current[1]] = 0;
 	$minX = min($minX,$current[0]);
 	$maxX = max($maxX,$current[0]);
 	$minY = min($minY,$current[1]);
