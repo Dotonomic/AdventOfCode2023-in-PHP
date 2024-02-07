3775cd5815c9641c49b05e731a964878a27a505a
Dotonomic <github@dotonomic.com>
2024-02-07 13:47:48 +0000

    Update 1.php

--- Solutions/1.php
+++ Solutions/1.php
@@ -3,15 +3,15 @@
 $total = 0;
 
 foreach(file('1input.txt') as $line) {
-    $line = preg_replace("/three/i","t3e",$line);
-    $line = preg_replace("/eight/i","e8t",$line);
-    $line = preg_replace("/two/i","t2o",$line);
-    $line = preg_replace("/one/i","o1e",$line);
-    $line = preg_replace("/four/i","f4r",$line);
-    $line = preg_replace("/five/i","f5e",$line);
-    $line = preg_replace("/six/i","s6x",$line);
-    $line = preg_replace("/seven/i","s7n",$line);
-    $line = preg_replace("/nine/i","n9n",$line);
+    $line = str_ireplace('three','t3e',$line);
+    $line = str_ireplace('eight','e8t',$line);
+    $line = str_ireplace('two','t2o',$line);
+    $line = str_ireplace('one','o1e',$line);
+    $line = str_ireplace('four','f4r',$line);
+    $line = str_ireplace('five','f5e',$line);
+    $line = str_ireplace('six','s6x',$line);
+    $line = str_ireplace('seven','s7n',$line);
+    $line = str_ireplace('nine','n9e',$line);
 
    $line = preg_replace("/([a-z]|\n)*/i","",$line);
    
